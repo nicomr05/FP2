2ea7f874b8e4770e12a2fd5a4643c3807729dfa6
Nico <nicolas.muniz@udc.es>
2024-02-15 11:44:28 +0100

    Practica 1

---
+++ materiales/pokemon.py
@@ -0,0 +1,43 @@
+"""
+TODO: Implement in this file the Pokemon hierarchy.
+"""
+from abc import ABC, abstractmethod
+
+class Pokemon(ABC):
+    '''
+    
+    '''
+    def __init__(self, name:str, level:int, strength:int, defense:int, hp:int, total_hp:int, agility:int, pokemon_type:str):
+        self._name = name
+        self._level = level
+        self._strength = strength
+        self._defense = defense
+        self._hp = hp
+        self._total_hp = total_hp
+        self._agility = agility
+        self._pokemon_type = pokemon_type
+        
+    def __str__(self):
+        
+        pass
+        
+    def basic_attack(self, p:Pokemon) -> int:
+        damage = self._strength - p._defense
+        self._hp = max(1, damage)
+        return damage
+    
+    def is_debilitated(self) -> bool:
+        if self._hp <= 0:
+            return True
+        else:
+            return False
+    
+    @abstractmethod
+    def effectiveness(self, p:Pokemon) -> int:
+        '''Clase abstracta dentro de Pokemon'''
+        pass
+        
+class WaterPokemon(Pokemon):
+    def __init__(self, name:str, level:int, strength:int, defense:int, hp:int, total_hp:int, agility:int, pokemon_type:str):
+        self.super().__init__(name, level, strength, defense, hp, total_hp, agility, pokemon_type)
+        
---
+++ materiales/trainer.py
@@ -0,0 +1,81 @@
+"""
+TODO: Implement in the file the Trainer class
+"""
+
+from pokemon import Pokemon
+
+class Trainer:
+    '''Clase de entrenador, es al que le pertenecen los pokémons.
+    
+    Clase en la que se recoge la lista de los nombres de los 6 pokémons pertenecientes al entrenador además
+    de comprobar si todos los pokémons del mismo están debilitados o no.
+ 
+    Attributes 
+    ---------- 
+    name : str
+         Nombre del entrenador.
+        pokemon : list 
+         Lista con los pokémons del entrenador.
+ 
+    Methods 
+    ------- 
+    all_debilitated(self, opponent): 
+        Método que comprueba si todos los pokémons están debilitados.
+    '''
+    def __init__(self, name:str, pokemon:list):
+        '''Asigna atributos al objeto. 
+ 
+        Parameters 
+        ---------- 
+        name : str 
+         Nombre del entrenador.
+        pokemon : list 
+         Lista con los pokémons del entrenador.
+ 
+        Returns 
+        ------- 
+        None.
+        '''
+        self._name = name
+        self._pokemon = pokemon
+        
+    def all_debilitated(self, opponent:Pokemon) -> bool:
+        '''Asigna True si todos los pokémons del entrenador fueron derrotados.
+ 
+        Returns 
+        ------- 
+        bool
+            Resultado de comprobar si todos los pokémons del entrenador están debilitados.
+        '''
+        estado = False
+        opponent = self._pokemon # ???
+        for pokemon in p:
+            if pokemon._hp <= 0:
+                estado = True
+            estado = estado # Revisar función (meter lógicos)
+        return estado
+        
+    #def select_initial_pokemon(self) -> Pokemon:    
+        
+    #def select_next_pokemon(self, p:Pokemon) -> Pokemon:
+        
+        
+    @property
+    def name(self) -> str:
+        return self._name
+    
+    @name.setter
+    def name(self, nuevo_nombre) -> str:
+        if isinstance(nuevo_nombre, str) and len(nuevo_nombre) > 0:
+            self._name = nuevo_nombre
+        else:
+            raise ValueError('El nombre no puede tener longitud 0.')
+    
+    @property
+    def pokemon(self) -> list:
+        return self._pokemon
+    
+    @pokemon.setter
+    def pokemon(self, nuevo_pokemon) -> list:
+        #Hacer setter de pokemons
+        pass
