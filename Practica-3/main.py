1837cb93f1adfcd2795e3fca579cf02f840d91ae
Nico <nicolas.muniz@udc.es>
2024-04-10 08:42:58 +0200

    Importaciones necesarias para el funcionamiento del programa

--- Practica-3/main.py
+++ Practica-3/main.py
@@ -2,3 +2,7 @@
 Nicolás Muñiz Rodríguez : nicolas.muniz@udc.es
 Pablo José Pérez Pazos : pablo.perez.pazos@udc.es
 '''
+
+from array_ordered_positional_list import ArrayOrderedPositionalList
+from linked_ordered_positional_list import LinkedOrderedPositionalList
+
