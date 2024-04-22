9362738b19d4fd23593b008f50378ff90e24bef5
Nico <nicolas.muniz@udc.es>
2024-04-22 11:47:01 +0200

    Inicio de Practica-4

---
+++ Practica-4/main.py
@@ -0,0 +1,24 @@
+'''
+Nicolás Muñiz Rodríguez : nicolas.muniz@udc.es
+Pablo José Pérez Pazos : pablo.perez.pazos@udc.es
+'''
+
+from sys import argv
+import pandas as pd
+
+class Pandas:
+    '''
+    '''
+    NotImplemented('Implementar Pandas.')
+
+def main() -> None:
+    '''
+    Función principal.
+    
+    Returns
+    -------
+    None
+    '''
+
+if __name__ == '__main__':
+    main()
