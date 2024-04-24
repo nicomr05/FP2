73e5d958bed5c735c714f8042304dc64bbdca7e9
Nico <nicolas.muniz@udc.es>
2024-04-24 12:08:51 +0200

    Arreglo menú + comentarios de métodos

--- Practica-4/materiales/main.py
+++ Practica-4/materiales/main.py
@@ -7,6 +7,7 @@ from avl_tree import AVL
 from curso import Curso
 from clase_pandas import Pandas
 from sys import argv
+from time import sleep
 
 class SimuladorCursos:
     '''
@@ -15,9 +16,18 @@ class SimuladorCursos:
 
     Methods
     -------
-    crear_arbol_cursos(self, texto:str) -> AVL
+    crear_arbol_cursos(self, texto:str) -> AVL:
+        Permite leer dos archivos de texto con la información de las los cursos
+        que se quieren organizar y los mete en dos árboles A y B respectivamente.
     
+    oferta_agregada(self) -> AVL:
+        Agrega los cursos realizados por cada una de las academias a un árbol común.
     
+    oferta_comun(self) -> AVL:
+        Agrega los cursos realizados únicamente en ambas academias a un árbol común.
+    
+    opcion_leer_archivos(self) -> bool:
+        Permite elegir entre leer o no leer los ficheros de texto.
     '''
     def crear_arboles_cursos(self) -> tuple[AVL,AVL]:
         '''
@@ -34,9 +44,9 @@ class SimuladorCursos:
         
         with open(argv[1], 'r', encoding='utf-8') as archivoA:
             texto1: str = archivoA.read()
-            lineas: str = str(texto1).split('\n')[1:len(lineas)-1] # Leemos el archivo
+            lineas: str = str(texto1).split('\n') # Leemos el archivo
             
-            for linea in lineas:
+            for linea in lineas[1:len(lineas)-1]:
                 partes_linea: str = str(linea).split(',')
 
                 nombre: str = str(partes_linea[0])
@@ -52,9 +62,9 @@ class SimuladorCursos:
 
         with open(argv[2], 'r', encoding='utf-8') as archivoB:
             texto2: str = archivoB.read()
-            lineas: str = str(texto2).split('\n')[1:len(lineas)-1]
+            lineas: str = str(texto2).split('\n')
             
-            for linea in lineas:
+            for linea in lineas[1:len(lineas)-1]:
                 partes_linea: str = str(linea).split(',')
 
                 nombre: str = str(partes_linea[0])
@@ -70,36 +80,56 @@ class SimuladorCursos:
 
         return (arbol_cursos_A, arbol_cursos_B)
     
+    
     def oferta_agregada(self) -> AVL:
         '''
-        '''
+        Método que agrega los cursos realizados por cada una de las academias a un árbol común.
         
+        Returns
+        -------
+        AVL
+         Árbol con los cursos de cada una de las academias.
+        '''
+        NotImplemented('Implementar oferta_agregada.')
+    
+
     def oferta_comun(self) -> AVL:
         '''
+        Método que agrega los cursos realizados únicamente en ambas academias a un árbol común.
+        
+        Returns
+        -------
+        AVL
+         Árbol con los cursos que aparecen en ambas academias.
         '''
+        NotImplemented('Implementar oferta_comun.')
     
-    def opcion_leer_archivos(self) -> None:
+    
+    def opcion_leer_archivos(self) -> bool:
         '''
         Método que permite elegir entre leer o no leer los ficheros de texto.
         
         Returns
         -------
-        None
+        bool
+         True si se decide leer los archivos.
+         False si se decide no leer los archivos.
         '''
         linea = '-'*50
-        texto = '¿Leer ficheros de los cursos A y B? (Y/N)'
+        texto = '¿Leer ficheros de los cursos A y B? (S/N)'
         
-        print(f'\n{linea}\n{texto}\n{linea}')
-        
-        eleccion: str = str(input('\nResponde aquí: '))
+        print(f'{linea}\n\n{texto}\n\n{linea}')
         
         while True:
-            if eleccion.upper() == 'Y':
-                print('\n  Leyendo ficheros...')
+            eleccion: str = str(input('\nResponde aquí: '))
+            
+            if eleccion.upper() == 'S':
+                print(f'\n{linea}\n\n  Leyendo ficheros...\n')
+                sleep(0.25)
                 return True
             
             elif eleccion.upper() == 'N':
-                print('\n  Saliendo...')
+                print(f'\n{linea}\n\n  Saliendo...\n')
                 return False
             
             else:
@@ -108,6 +138,12 @@ class SimuladorCursos:
         
     def menu(self) -> None:
         '''
+        Método que imprime por pantalla el menú de opciones para navegar entre los cursos y le permite escoger
+        a un usuario entre 3 opciones de visualización/filtrado, además de la opción de poder salir del menú.
+        
+        Returns
+        -------
+        None
         '''
         linea = '-'*50
         
@@ -145,6 +181,7 @@ class SimuladorCursos:
             elif respuesta not in opciones:
                 print('La opción no se encuentra en el menú.')
 
+
 def main() -> None:
     '''
     Función principal que lee el archivo de texto y crea los objetos Curso.
@@ -153,8 +190,10 @@ def main() -> None:
     -------
     None
     '''
-    pass
-        
+    simulador: SimuladorCursos = SimuladorCursos()
+    simulador.menu()        
+
+
 
 if __name__ == '__main__':
     main()
